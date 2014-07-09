f0528e4acaa3997d0d9c89810480c639426103e7
Maxim Danilov <sambo_krg@mail.ru>
2014-07-10 02:56:21 +0600

    addParser

--- src/main/java/com/epam/mv/main/Main.java
+++ src/main/java/com/epam/mv/main/Main.java
@@ -1,5 +1,14 @@
 package com.epam.mv.main;
 
 
+import com.epam.mv.parser.Parser;
+
 public class Main {
+    public static void main(String [] arg){
+        String s = "I call the reset the ‘following four’ quipped Shatzkin, who pointed out that even if the four remaining .\n"
+                +"\n"+
+                "Shatzkin countered, “I ultimately think we’ll see that general trade publishing is going to consolidate into.";
+
+        Parser parser = new Parser();
+    }
 }
